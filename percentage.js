1a1e95f38babb1b06053e965430e157f1fcbdeb0
Anshika290 <anshikasaxena200@gmail.com>
2022-03-29 11:57:56 +0530

    my second commit

---
+++ percentage.js
@@ -0,0 +1,20 @@
+var sub1 = 60;
+var sub2 = 90;
+var sub3 = 86;
+var sub4 = 55;
+var sub5 = 78;
+var sum = sub1+sub2+sub3+sub4+sub5;
+var percentage = sum*100/500;
+var division;
+console.log("percentage is" + percentage)
+if(percentage>=60)
+{
+    console.log("you are passed by first division" + division);
+}
+else if(percentage>=33)
+{
+    console.log("you are passed by second division" + division);
+}
+else{
+    console.log("you are passed by third division" + division); 
+}
